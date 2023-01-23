2b210b4ec2a07d64872ab53f61613a07c59a4430
SJohnRose <shamini.johnrose@gmail.com>
2023-01-23 23:58:11 +1100

    Added code to script.js to display current weather for any city

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,29 @@
 var searchButton = document.getElementById('search-btn');
-var APIKey = "e55ee97b6b016b9b14fab1caec587add";
-var city = "Atlanta";
+var cityInputEl = document.getElementById("cityname");
 
 function getApi() {
-   var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=-37.910760&lon=144.622444&appid=e55ee97b6b016b9b14fab1caec587add';
-// var requestUrl = "https://api.github.com/orgs/nodejs/repos";
-alert(requestUrl);
-fetch(requestUrl)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+    var searchCity = cityInputEl.value;
+    console.log(searchCity); 
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?appid=e55ee97b6b016b9b14fab1caec587add&q='+ searchCity;
+    fetch(requestUrl)
+        .then((response) => response.json())
+        .then((data) => populateData(data));
     
 }
 
 searchButton.addEventListener('click', getApi);
+
+function setValueToElement(id, value) {
+    document.getElementById(id).textContent = value;
+}
+
+function populateData(data) {
+    console.log(data);
+    console.log(data.list[0].weather[0].icon);
+    var cityDate= data.city.name + "   (" + data.list[0].dt_txt + ")";
+    setValueToElement('city-name', cityDate);
+    document.getElementById('weather-icon').src="http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png";
+    setValueToElement('city-temp-1', "Temp:" + data.list[0].main.temp + " 0 C");
+    setValueToElement('city-wind-1', "Wind:" + data.list[0].wind.speed + " MPH");
+    setValueToElement('city-humidity-1', "Humidity:" + data.list[0].main.humidity + " %");
+}
