bc8d4ca000d958e7aff926dfd39bf361f508f60d
SJohnRose <shamini.johnrose@gmail.com>
2023-01-24 16:00:15 +1100

    Fixed error showing five day forecast in script.js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,14 @@
 var searchButton = document.getElementById('search-btn');
 var cityInputEl = document.getElementById("cityname");
-var historySectionEl = document.getElementById("search-history-section");
-var clicked = false;
+const historySectionEl = document.getElementById("search-history-section");
+const fiveDayDivEl = document.getElementById("five-day-section");
 
+showHistoryLocal();
 
 // Function to get weather data from Open Weather Map API
 function getApi() {
     var cityName = cityInputEl.value;
     var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?appid=e55ee97b6b016b9b14fab1caec587add&q='+ cityName + "&units=metric";
-    clicked = true;
-    showHistoryLocal();
     fetch(requestUrl)
         .then((response) => response.json())
         .then((data) => populateData(data));
@@ -26,7 +25,9 @@ function setValueToElement(id, value) {
 
 // Extracting the required temperature, wind and humidity information from weather JSON
 function populateData(data) {
-    var cityDate= data.city.name + "   (" + data.list[0].dt_txt + ")";
+    fiveDayDivEl.style.visibility = "visible";
+    var date = formatDate(String(data.list[0].dt_txt).substring(0,10));
+    var cityDate= data.city.name + "   (" + date + ")";
     setValueToElement('city-name', cityDate);
     document.getElementById('weather-icon').src="http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png";
     setValueToElement('city-temp-1', "Temp:  " + data.list[0].main.temp + '  \u00B0' + "C");
@@ -36,10 +37,23 @@ function populateData(data) {
     
 }
 
+//Change the format of the date to dd/mm/yyyy
+function formatDate(oldDate) {
+    var day = oldDate.substring(8,10);
+    var month = oldDate.substring(5,7);
+    var year = oldDate.substring(0,4);
+    return(`${day}/${month}/${year}`);
+
+}
+
+
 // Store the cities searched in local storage
 function storeHistoryLocal(cityName) {
     var citiesLocal = localStorage.getItem("City-History");
     citiesLocal = citiesLocal ? JSON.parse(citiesLocal) : [];
+    if(citiesLocal.length > 10) {
+        localStorage.clear();
+    }
     if(!citiesLocal.includes(cityName)) {
         citiesLocal.push(cityName);
         localStorage.setItem("City-History", JSON.stringify(citiesLocal));
@@ -48,7 +62,6 @@ function storeHistoryLocal(cityName) {
 
 // Retrieve search history from local storage and display in web page
 function showHistoryLocal() {
-    if(clicked==true) {
     var citiesLocal = localStorage.getItem("City-History");
     citiesLocal = citiesLocal ? JSON.parse(citiesLocal) : [];
     var numCities = citiesLocal.length;
@@ -63,28 +76,32 @@ function showHistoryLocal() {
         cityEl.style.marginBottom = "20px";
         cityEl.style.width = "450px";
         cityEl.style.height = "40px";
+        cityEl.style.fontWeight = "bold";
+        cityEl.style.border = "none";
         cityEl.textContent = citiesLocal[i];
         historySectionEl.appendChild(cityEl);
     }  
 }    
-}
+
 
 
 historySectionEl.addEventListener('click', function (event) {
-    if(event.target.textContent != "Search") {
+    if(event.target.textContent !== "Search") {
         console.log(event.target.textContent);
-        cityInputEl.textContent = event.target.textContent;
-        //getApi;
+        cityInputEl.value = event.target.textContent;
+        getApi();
     }
 });
 
+// Display weather for next five days
 function showFiveDayWeather(data) {
+    fiveDayDivEl.style.visibility = "visible";
     for (var i=1; i<=5;i++) {
-        var cityDate= data.list[7*i].dt_txt;
-        setValueToElement('date'+i, cityDate);
-        document.getElementById('weather-icon-day'+i).src="http://openweathermap.org/img/wn/" + data.list[7*i].weather[0].icon + ".png";
-        setValueToElement('temp-day'+i, "Temp:  " + data.list[7*i].main.temp + '  \u00B0' + "C");
-        setValueToElement('wind-day'+i, "Wind:  " + data.list[7*i].wind.speed + " MPH");
-        setValueToElement('humidity-day'+i, "Humidity:  " + data.list[7*i].main.humidity + " %");
+        var cityDate= data.list[7*i+3].dt_txt;
+        setValueToElement('date'+i, formatDate(String(cityDate).substring(0,10)));
+        document.getElementById('weather-icon-day'+i).src="http://openweathermap.org/img/wn/" + data.list[7*i+3].weather[0].icon + ".png";
+        setValueToElement('temp-day'+i, "Temp:  " + data.list[7*i+3].main.temp + '  \u00B0' + "C");
+        setValueToElement('wind-day'+i, "Wind:  " + data.list[7*i+3].wind.speed + " MPH");
+        setValueToElement('humidity-day'+i, "Humidity:  " + data.list[7*i+3].main.humidity + " %");
     }
 }
